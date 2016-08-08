2b2bee115b7fcaeab820246f2471a724421aaba1
andrew.monteith <andrew.monteith@kiandra.com.au>
2016-08-08 13:47:30 +1000

    added proper install

---
+++ install.sh
@@ -0,0 +1,5 @@
+ln -sf ~/.console-settings/.alias ~/.alias
+ln -sf ~/.console-settings/.bashrc ~/.bashrc
+ln -sf ~/.console-settings/.profile ~/.profile
+ln -sf ~/.console-settings/.vars ~/.vars
+ln -sf ~/.console-settings/.zshrc ~/.zshrc
